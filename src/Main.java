7ef5b66d3fded7d1c5597906ec61d5d883a25eaf
Galina_c <galina__k@mail.ru>
2022-09-09 00:02:28 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1.1
+        System.out.println("Задание 1.1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(+i);
+        }
+        //Задание 1.2
+        System.out.println("Задание 1.2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(+i);
+        }
+        //Задание 1.3
+        System.out.println("Задание 1.3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(+i);
+        }
+        //Задание 1.4
+        System.out.println("Задание 1.4");
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(+i);
+        }
+        //Задание 2.1
+        System.out.println("Задание 2.1");
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(+i + " - год является високосным");
+        }
+        //Задание 2.2
+        System.out.println("Задание 2.2");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(+i);
+        }
+        //Задание 2.3
+        System.out.println("Задание 2.3");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(+i);
+        }
+        //Задание 3.1
+        System.out.println("Задание 3.1");
+        int fee = 29000;
+        int accumulation = 0;
+        for (int i = 1; i <= 12; i++) {
+            accumulation = accumulation + fee;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + accumulation);
+        }
+        //Задание 3.2
+        System.out.println("Задание 3.2");
+        int fee1 = 29000;
+        int accumulation1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            accumulation1 = accumulation1 + fee1;
+            accumulation1 = accumulation1 + accumulation1 / 100;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + accumulation1);
+        }
 
     }
 }
